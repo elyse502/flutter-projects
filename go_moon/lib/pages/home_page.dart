c1434feb09e638a783cff3ec64dd7fa614cbe88d
elyse502 <elyseniyibizi502@gmail.com>
2025-07-15 07:32:38 +0200

    feat: Add how to Acces Device Height and Width

--- go_moon/lib/pages/home_page.dart
+++ go_moon/lib/pages/home_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  late double _deviceHeight, _deviceWidth;
+
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SafeArea(child: Container(child: _pageTitle())),
+      body: SafeArea(
+        child: Container(
+          // color: Colors.red,
+          height: _deviceHeight,
+          width: _deviceWidth,
+          child: _pageTitle(),
+        ),
+      ),
     );
   }
 
