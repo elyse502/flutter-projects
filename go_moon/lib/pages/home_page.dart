df24dbfc36d41acbe6c9920fc7c578579d56dd79
elyse502 <elyseniyibizi502@gmail.com>
2025-07-15 19:47:49 +0200

    feat: Add all about Widget Styling

--- go_moon/lib/pages/home_page.dart
+++ go_moon/lib/pages/home_page.dart
@@ -50,12 +50,24 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _destinationDropDownWidget() {
-    List<DropdownMenuItem<String>> _items =
-        ['James Webb Station', 'Preneure Station'].map((e) {
-          return DropdownMenuItem(child: Text(e), value: e);
-        }).toList();
+    List<String> _items = ['James Webb Station', 'Preneure Station'];
     return Container(
-      child: DropdownButton(onChanged: (_) {}, items: _items),
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(53, 53, 53, 1.0),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: DropdownButton(
+        value: _items.first,
+        onChanged: (_) {},
+        items: _items.map((e) {
+          return DropdownMenuItem(child: Text(e), value: e);
+        }).toList(),
+        underline: Container(),
+        dropdownColor: const Color.fromRGBO(53, 53, 53, 1.0),
+        style: const TextStyle(color: Colors.white),
+      ),
     );
   }
 }
