2ca0eb38664205c3d373da3cfb700f1a4df34116
elyse502 <elyseniyibizi502@gmail.com>
2025-07-15 07:11:33 +0200

    feat: Add all about Adding Text Widget

--- go_moon/lib/pages/home_page.dart
+++ go_moon/lib/pages/home_page.dart
@@ -5,14 +5,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Colors.red,
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage("assets/images/astro_moon.png"),
-          ),
+    return Scaffold(body: _pageTitle());
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 70,
+        fontWeight: FontWeight.w800,
+      ),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage("assets/images/astro_moon.png"),
         ),
       ),
     );
