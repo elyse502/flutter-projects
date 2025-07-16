4ac0ff400fefecede5f8e8b3a4576fe6290688e5
elyse502 <elyseniyibizi502@gmail.com>
2025-07-16 07:36:06 +0200

    feat: Add Stack and Alignment Widget

--- go_moon/lib/pages/home_page.dart
+++ go_moon/lib/pages/home_page.dart
@@ -17,11 +17,19 @@ class HomePage extends StatelessWidget {
           height: _deviceHeight,
           width: _deviceWidth,
           padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [_pageTitle(), _bookRideWidget()],
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [_pageTitle(), _bookRideWidget()],
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: _astroImageWidget(),
+              ),
+            ],
           ),
         ),
       ),
@@ -41,6 +49,8 @@ class HomePage extends StatelessWidget {
 
   Widget _astroImageWidget() {
     return Container(
+      height: _deviceHeight * 0.50,
+      width: _deviceWidth * 0.65,
       decoration: const BoxDecoration(
         image: DecorationImage(
           fit: BoxFit.fill,
