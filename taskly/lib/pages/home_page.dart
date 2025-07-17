14c7f5b55313b8f4b5f84cc2860fa9e88b6e3e3e
elyse502 <elyseniyibizi502@gmail.com>
2025-07-17 07:45:17 +0200

    feat: Add all about TextField and SetState In Flutter

--- taskly/lib/pages/home_page.dart
+++ taskly/lib/pages/home_page.dart
@@ -10,12 +10,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late double _deviceHeight, _deviceWidth;
 
+  String? _newTaskContent;
+
   _HomePageState();
 
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+    // print("Input Value: $_newTaskContent");
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: _deviceHeight * 0.15,
@@ -46,10 +49,31 @@ class _HomePageState extends State<HomePage> {
 
   Widget _addTaskButton() {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: _displayTaskPopup,
       // shape: CircleBorder(),
       backgroundColor: Colors.red,
       child: Icon(Icons.add, color: Colors.white),
+    );
+  }
+
+  void _displayTaskPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext _context) {
+        return AlertDialog(
+          title: Text("Add New Task!"),
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          content: TextField(
+            onSubmitted: (_value) {},
+            onChanged: (_value) {
+              setState(() {
+                _newTaskContent = _value;
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
