8bcc442d0d9d50c4cc10766a8e8773c55cb3957b
elyse502 <elyseniyibizi502@gmail.com>
2025-07-18 17:35:53 +0200

    feat: Add all about Animated Widgets In Flutter

--- animdo/lib/pages/home_page.dart
+++ animdo/lib/pages/home_page.dart
@@ -8,8 +8,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double _buttonRadius = 100;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [_pageBackground(), _circularAnimationButton()],
+        ),
+      ),
+    );
+  }
+
+  Widget _pageBackground() {
+    return Container(color: Colors.blue);
+  }
+
+  Widget _circularAnimationButton() {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _buttonRadius += _buttonRadius == 200 ? -100 : 100;
+          });
+        },
+        child: AnimatedContainer(
+          duration: Duration(seconds: 2),
+          curve: Curves.bounceInOut,
+          height: _buttonRadius,
+          width: _buttonRadius,
+          decoration: BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.circular(_buttonRadius),
+          ),
+          child: Center(
+            child: Text("Basic!", style: TextStyle(color: Colors.white)),
+          ),
+        ),
+      ),
+    );
   }
 }
